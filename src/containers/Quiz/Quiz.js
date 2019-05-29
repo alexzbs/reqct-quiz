9edfdbe37a6883389deb01bd11feb5f27509b868
alexzbs <zybinskii@gmail.com>
2019-05-29 11:54:43 +0300

    first commit

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import classes from "./Quiz.css";
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
+
+class Quiz extends Component {
+  state = {
+    activeQuestion: 0,
+    quiz: [
+      {
+        question: "Какого цвета небо?",
+        rightAnswerId: 2,
+        id: 1,
+        answers: [
+          { text: "черный", id: 1 },
+          { text: "синий", id: 2 },
+          { text: "красный", id: 3 },
+          { text: "зеленый", id: 4 }
+        ]
+      },
+      {
+        question: "В каком году основали Санкт-Петербург?",
+        rightAnswerId: 3,
+        id: 2,
+        answers: [
+          { text: "1700", id: 1 },
+          { text: "1705", id: 2 },
+          { text: "1703", id: 3 },
+          { text: "1803", id: 4 }
+        ]
+      }
+    ]
+  };
+
+  onAnswerClickHandler = answerId => {
+    console.log(answerId);
+    this.setState({
+      activeQuestion: this.state.activeQuestion + 1
+    })
+  };
+
+  render() {
+    return (
+      <div className={classes.Quiz}>
+        <div className={classes.QuizWrapper}>
+          <h1>Ответьте на все вопросы</h1>
+          <ActiveQuiz
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
+            onAnswerClick={this.onAnswerClickHandler}
+            quizLength = {this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Quiz;
