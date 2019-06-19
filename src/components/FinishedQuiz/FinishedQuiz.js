b3641b9b30b814bc8eae3826cc962cbab2517d1b
alexzbs <zybinskii@gmail.com>
2019-06-19 10:35:51 +0300

    P8L5

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './FinishedQuiz.css'
 import Button from '../UI/Button/Button'
+import { Link } from 'react-router-dom'
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -15,7 +16,7 @@ const FinishedQuiz = props => {
   return (
     <div className={classes.FinishedQuiz}>
       <ul>
-        { props.quiz.map((quizItem, index) => {
+        {props.quiz.map((quizItem, index) => {
           const cls = [
             'fa',
             props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
@@ -32,14 +33,16 @@ const FinishedQuiz = props => {
             </li>
           )
 
-        }) }
+        })}
       </ul>
 
       <p>Правильно {successCount} из {props.quiz.length}</p>
 
       <div>
         <Button onClick={props.onRetry} type="primary">Повторить</Button>
-        <Button type="success">Перейти в список тестов</Button>
+        <Link to="/">
+          <Button type="success">Перейти в список тестов</Button>
+        </Link>
       </div>
     </div>
   )
