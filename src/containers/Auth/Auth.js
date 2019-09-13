e5d50bc3c2376d3e1f23e89b2b764ab0e2effe5e
alexzbs <aazybinsky@rttv.ru>
2019-09-13 18:27:14 +0300

    add Input validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
-import { log } from "util";
+import is from 'is_js'
 
 export default class Auth extends Component {
   state = {
@@ -34,19 +34,46 @@ export default class Auth extends Component {
     }
   };
 
-  loginHandler = () => {};
-  registerHandler = () => {};
+  loginHandler = () => { };
+  registerHandler = () => { };
 
   submitHandler = event => event.preventDefault();
 
+  validateControl(value, validation) {
+    if(!validation) { return true }
+    let isValid = true
+
+    if(validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+    if(validation.email) {
+      isValid = is.email(value) && isValid
+    }
+    if(validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
+    }
+
+    return isValid
+  }
+
   onChangeHandler = (event, controlName) => {
-    console.log(`${controlName}: `, event.target.value);
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
+
+    control.value = event.target.value
+    control.touched = true
+    control.valid = this.validateControl(control.value, control.validation)
+
+    formControls[controlName] = control
+    this.setState({
+      formControls
+    })
   }
 
   renderInputs() {
     return Object.keys(this.state.formControls).map((controlName, index) => {
       const control = this.state.formControls[controlName]
-      return <Input 
+      return <Input
         key={controlName + index}
         type={control.type}
         value={control.value}
@@ -54,8 +81,8 @@ export default class Auth extends Component {
         touched={control.touched}
         label={control.label}
         shouldValidate={!!control.validation}
-        errorMessage= {control.errorMessage}
-        onChange={event => this.onChangeHandler(event,controlName)}
+        errorMessage={control.errorMessage}
+        onChange={event => this.onChangeHandler(event, controlName)}
       />;
     });
   }
