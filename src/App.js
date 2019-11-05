cba1811cfa14426d7c34bd26937106d0de71ffee
alexzbs <zybinskii@gmail.com>
2019-11-05 22:43:20 +0300

    P12L8

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { autoLogin } from './store/actions/auth'
 
 class App extends Component {
   componentDidMount() {
-    this.props.authLogin()
+    this.props.autoLogin()
   }
   
   render() {
@@ -19,7 +19,7 @@ class App extends Component {
       <Switch>
         <Route path="/auth" component={Auth} />
         <Route path="/quiz/:id" component={Quiz} />
-        <Route path="/" component={QuizList} />
+        <Route path="/" exact component={QuizList} />
         <Redirect to='/' />
       </Switch>
     )
@@ -27,8 +27,8 @@ class App extends Component {
       routes = <Switch>
         <Route path="/quiz-creator" component={QuizCreator} />
         <Route path="/quiz/:id" component={Quiz} />
-        <Route path="/" component={QuizList} />
         <Route path="/logout" component={Logout} />
+        <Route path="/" exact component={QuizList} />
         <Redirect to='/' />
       </Switch>
     }
@@ -47,7 +47,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    authLogin: () => dispatch(autoLogin())
+    autoLogin: () => dispatch(autoLogin())
   }
 }
-export default withRouter(connect(mapStateToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
